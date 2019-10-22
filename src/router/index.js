a24e00483ac5e5b6b0cb7e6ad977ca5b61586bfa
warm113 <1135375289@qq.com>
2019-10-22 18:58:07 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+/*
+路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Msite from "../pages/MSite/Msite";
+import Search from "../pages/Search/Search";
+import Profile from "../pages/Profile/Profile";
+import Order from "../pages/Order/Order";
+import Login from "../pages/Login/Login";
+//生命使用插件
+Vue.use(VueRouter)
+export default new VueRouter({
+  //所有路由
+  routes:[
+
+    {
+      path:'/msite',
+      component:Msite,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/search',
+      component:Search,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/profile',
+      component:Profile,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/order',
+      component:Order,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/',
+      redirect:'/msite'
+    }
+  ]
+})
